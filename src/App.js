da2be37df385f8256570affea68c4ac73c0c918a
Lauren Piontek <piontekle@gmail.com>
2018-12-13 13:58:05 -0500

    Checkpoint 7 React states and props: completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {description: 'Walk the cat', isCompleted: true },
+        {description: 'Throw the dishes away', isCompleted: false },
+        {description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          { this.state.todos.map((todo, index) =>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted }/>
+          )}
+        </ul>
       </div>
     );
   }
